3cee4fe867cdabc3a342a61e385269678ba1fcdb
Antonio Elton Rios Lima <antonioelton@outlook.com>
2026-03-28 17:31:00 -0300

    Alteração - Reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,22 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const { timestamp } = require('rxjs');
 
 module.exports = defineConfig({
+  projectId: '7b8i5g',
   allowCypressEnv: false,
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+    video: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reporterDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: 'mmddyyyy_HHMMss',
     },
   },
 });
