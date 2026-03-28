9af7d1d2ddc8e6cd86e5ea946cda641a6ccb0d7f
Antonio Elton Rios Lima <antonioelton@outlook.com>
2026-03-28 15:19:50 -0300

    Teste falha API usando stub

--- cypress/e2e/login-falha.cy.js
+++ cypress/e2e/login-falha.cy.js
@@ -1,5 +1,33 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Login com falha', () => {
+  beforeEach(() => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('[data-test="login-button"]').click();
+    cy.intercept(
+      'POST',
+      'https://adopet-api-i8qu.onrender.com/adotante/login',
+      {
+        statusCode: 400,
+      },
+    ).as('stubPost');
+  });
+
+  it('Deve exibir mensagem de erro ao tentar fazer login com credenciais inválidas', () => {
+    /* cy.get('[data-test="input-loginEmail"]').type('elton@elton.com');
+    cy.get('[data-test="input-loginPassword"]').type('Teste123');
+    cy.get('[data-test="submit-button"]').click(); */
+
+    // Refatorado
+    cy.login('elton@elton.com', 'Teste123');
+    cy.contains('Falha no login. Consulte suas credenciais.').should(
+      'be.visible',
+    );
+  });
+
+  it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+    cy.login('antonioelton81@email.com', 'Senha123');
+    cy.wait('@stubPost');
+    cy.contains('Falha no login. Consulte suas credenciais.').should(
+      'be.visible',
+    );
+  });
+});
